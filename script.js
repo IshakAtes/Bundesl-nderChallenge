6528c9ff10d897dcd97303fb25c4ab1ed014083e
IshakAtes <ishakfeuer@gmail.com>
2022-09-20 11:16:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let bundeslaender = [];
+async function init() {
+    await getData();
+    render();
+}
+
+async function getData(){
+    let response = await fetch('./bundesland.json')
+    bundesldr = await response.json();
+    bundeslaender.push(bundesldr)
+    // for (let i = 0; i < bundesldr.length; i++) {
+    //     const element = bundesldr[i];
+    //     bundeslaender.push(element['name'], element['population'])
+    // }
+    
+    console.log(bundeslaender)
+}
+
+function render(){
+    let renderLand = document.getElementById('renderContainer');
+    document.getElementById('headline').innerHTML = 'BUNDESLÄNDER';
+    renderLand.innerHTML = '';
+
+    for (let i = 0; i < bundeslaender[0].length; i++) {
+        const element = bundeslaender[0][i];
+        renderLand.innerHTML += `
+            <a class="landBorder" href="${element['url']}" target="_blank">
+                <div class="landContainer">
+                    <div id="landContent" class="landContentStyling">${element['name']}</div>
+                    <span id="population" class="populationStyling">${element['population']} Millionen</span>
+                </div>
+            </a>
+        `;
+    }
+}
